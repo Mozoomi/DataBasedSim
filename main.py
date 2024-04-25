7001e811e24d7c67edea62e235bbe28744191170
Mozoomi <moza12tv@gmail.com>
2024-04-24 20:53:36 -0400

    population_distr

---
+++ main.py
@@ -0,0 +1,9 @@
+from prereq import SystemSettings
+
+
+pop_size = int(input("Enter population size: "))
+gender_dist = float(input("Enter gender distribution (percentage of males): "))
+age_min = int(input("Enter the minimum age: "))
+age_max = int(input("Enter the max age: "))
+settings = SystemSettings(pop_size, gender_dist, age_max, age_min)
+settings.saveData()
